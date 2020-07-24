ee43fd69b685e99f4eb816186c85949d0210254c
jigarvakil <59017256+jigarvakil@users.noreply.github.com>
2020-07-24 00:11:26 -0700

    Add files via upload

---
+++ resume.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Jigar Vakil-Resume</title>
+
+
+<link type="text/css" rel="stylesheet" href="style.css">
+
+</head>
+<body id="top">
+<div id="cv" class="instaFade">
+	<div class="mainDetails">
+		<div id="headshot">
+			<img src="1.jpg" alt="Jigar Vakil" />
+		</div>
+		
+		<div id="name">
+			<h1>Jigar Vakil</h1> <p>Address: 48, Silver Park,Barbodhan Village, Surat</p>
+			
+		</div>
+		
+		<div id="contactDetails">
+            <ul>
+				<li>email: <a href="mailto:jigarvakil011@gmail.com" target="_blank">Jigarvakil011@gmail.com</a></li>
+				<li>mobile: 9824619885</li>
+				<li>Birth Date: 11-04-1999</li>
+			</ul>
+		</div>
+		<div class="clear"></div> 
+	</div>
+	
+	<div id="mainArea">
+		
+		
+		
+		<section>
+			<div class="sectionTitle">
+				<h1>Work Experience</h1>
+			</div>
+			
+			<div class="sectionContent">
+				<div>
+					<h2>PhP Devloper</h2>
+					<p class="subDetails">April 2019 - April 2020</p>
+					<p>At :CloudZsoft, Netx-iit, 1st floor ,balaji market, balaji road,near lalgate, surat </p>
+				</div>				
+			</div>
+			<div class="clear"></div>
+		</section>
+		
+		
+		<section>
+			<div class="sectionTitle">
+				<h1>Key Skills</h1>
+			</div>
+			
+			<div class="sectionContent">
+				<ul class="keySkills">
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>JAVASCRIPT</li>
+					<li>PHP</li>
+					<li>CodeIgniter</li>
+				</ul>
+			</div>
+			<div class="clear"></div>
+		</section>
+		
+		
+		<section>
+			<div class="sectionTitle">
+				<h1>Education</h1>
+			</div>
+			
+			<div class="sectionContent">
+				<div>
+					<h3>SSC (GSEB) </h3>
+					<p>School: L.P.Savani Vidhyabhavan</p>
+					<p>Year- 2014 <br> Percantage: 66%</p>
+				</div>
+                
+                <div>
+					<h3>HHC (GSHEB) </h3>
+					<p>School: Sanskar Bharti Vidhyala</p>
+					<p>Year- 2016 <br> Percantage: 66%</p>
+                </div>
+                
+                <div>
+					<h3>BCA (VNSGU) </h3>
+					<p>College: C.B.Paltel Computer College</p>
+					<p>Year- 2019 <br> CGPA: 7.74 </p>
+				</div>
+				
+			</div>
+			<div class="clear"></div>
+		</section>
+		
+    </div>
+    <hr>
+    <div style="margin-left:20px">
+
+    <h3>Declaration</h3>
+            <p>
+            I hereby declare that the details furnished above are true and correct to the best of my knowledge and belief.</p>
+<br>
+<br>
+    </div>
+</div>
+</body>
+</html>
